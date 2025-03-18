c8f616a2009429eb595d0dbdb57fd3e5ab9de09a
orfeo42 <lucio.rosadini87@gmail.com>
2025-03-18 17:15:18 +0100

    - add LongestNiceSubarray

--- main.go
+++ main.go
@@ -1,11 +1,33 @@
 package main
 
-import (
-	"fmt"
-	"test-go/leetcode"
-)
+import "fmt"
 
 func main() {
-	result := leetcode.RepairCars([]int{4, 2, 3, 1}, 10)
-	fmt.Println(result)
+	nums := []int{135745088, 609245787, 16, 2048, 2097152}
+	fmt.Println(longestNiceSubarray(nums))
+	for end := 1; end < len(nums); end++ {
+		fmt.Printf("start: %d, end: %d, res: %d\n", nums[end-1], nums[end], nums[end]&nums[end-1])
+	}
+}
+
+func longestNiceSubarray(nums []int) int {
+	bitmask := 0
+	start := 0
+	maxLen := 0
+	for end := 0; end < len(nums); end++ {
+		for bitmask&nums[end] != 0 {
+			bitmask ^= nums[start]
+			start++
+		}
+		bitmask |= nums[end]
+		maxLen = maxInt(maxLen, end-start+1)
+	}
+	return maxLen
+}
+
+func maxInt(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
 }
