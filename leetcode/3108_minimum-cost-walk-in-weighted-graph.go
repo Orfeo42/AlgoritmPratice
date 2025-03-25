659e04ce4007e2c6443a677bbcaae833022a00bb
orfeo42 <lucio.rosadini87@gmail.com>
2025-03-25 18:12:45 +0100

    leetcode --

--- leetcode/3108_minimum-cost-walk-in-weighted-graph.go
+++ leetcode/3108_minimum-cost-walk-in-weighted-graph.go
@@ -1,6 +1,8 @@
 package leetcode
 
-//https://leetcode.com/problems/minimum-cost-walk-in-weighted-graph/
+import "math"
+
+// https://leetcode.com/problems/minimum-cost-walk-in-weighted-graph/
 
 type Edge struct {
 	Weight int
@@ -11,43 +13,63 @@ func MinimumCost(n int, edges [][]int, query [][]int) []int {
 	graph := map[int][]Edge{}
 
 	for _, e := range edges {
-		if _, exists := graph[e[0]]; !exists {
-			graph[e[0]] = []Edge{}
-		}
-		graph[e[0]] = append(graph[e[0]], Edge{
-			Weight: e[2],
-			Target: e[1],
-		})
-
-		if _, exists := graph[e[1]]; !exists {
-			graph[e[1]] = []Edge{}
-		}
-		graph[e[1]] = append(graph[e[1]], Edge{
-			Weight: e[2],
-			Target: e[0],
-		})
+		graph[e[0]] = append(graph[e[0]], Edge{Weight: e[2], Target: e[1]})
+		graph[e[1]] = append(graph[e[1]], Edge{Weight: e[2], Target: e[0]})
 	}
-	var result []int
+	result := make([]int, len(query))
 
-	for _, q := range query {
+	for i, q := range query {
 		if _, exists := graph[q[0]]; !exists {
-			result = append(result, -1)
+			result[i] = -1
 			continue
 		}
 		if _, exists := graph[q[1]]; !exists {
-			result = append(result, -1)
+			result[i] = -1
 			continue
 		}
-		/*links := graph[q[0]]
-		minLength := math.MaxInt
-		for _, l := range links {
-			length := math.MaxInt
-
-		}*/
-
-		result = append(result, 1)
+		visited := make(map[int]bool)
+		result[i] = findPath(q[0], q[1], graph, math.MaxInt, visited)
 	}
 
 	return result
 
+}
+
+func minInt(n1, n2 int) int {
+	if n1 < n2 {
+		return n1
+	}
+	return n2
+}
+
+func minIntArr(arr []int) int {
+	minVal := math.MaxInt
+	for _, n := range arr {
+		if minVal > n {
+			minVal = n
+		}
+	}
+	return minVal
+}
+
+func findPath(cur, target int, graph map[int][]Edge, cost int, visited map[int]bool) int {
+	if cur == target {
+		return cost
+	}
+	visited[cur] = true
+	minCost := math.MaxInt
+
+	for _, e := range graph[cur] {
+		if visited[e.Target] {
+			continue
+		}
+		newCost := findPath(e.Target, target, graph, cost&e.Weight, visited)
+		minCost = minInt(minCost, newCost)
+	}
+
+	visited[cur] = false
+	if minCost == math.MaxInt {
+		return -1
+	}
+	return minCost
 }
--- main.go
+++ main.go
@@ -2,6 +2,4 @@ package main
 
 import "test-go/leetcode"
 
-func main() {
-	leetcode.CheckValidCuts(5, [][]int{{1, 0, 5, 2}, {0, 2, 2, 4}, {3, 2, 5, 3}, {0, 4, 4, 5}})
-}
+func main() {}
